89c4f3335e0a36cd20adc34e847184ecdc40ddd5
Amanda Moura <amanda.ms4@hotmail.com>
2024-03-18 11:32:46 -0300

    Alterando o texto de acerto pela 5ª vez

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou Mizeravi 4!');
+        exibirTextoNaTela('h1', 'Acertou Mizeravi 5!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
